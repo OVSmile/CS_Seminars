d61da3b4d36e6c987686c1d419663d7a32946597
Oleg Smile <valsov-9988@mail.ru>
2022-10-23 14:30:41 +0300

    Task_47 Homework

---
+++ CS_Seminar7/Task_47_Homework/Task_47_Homework.cs
@@ -0,0 +1,41 @@
+﻿// Задайте двумерный массив размером m x n,
+// заполненный случайными вещественными числами.
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+double[,] Array = GetArray(rows, columns, 0, 10);
+PrintArray(Array);
+
+
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = Math.Round((new Random().NextDouble()* 2 - 1) * 10, 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
