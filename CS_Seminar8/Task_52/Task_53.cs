33ab9d8e95abfbc7486585cc989774e340074f96
Oleg Smile <valsov-9988@mail.ru>
2022-11-09 20:15:45 +0300

    Seminar 8

---
+++ CS_Seminar8/Task_52/Task_53.cs
@@ -0,0 +1,53 @@
+﻿Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+int[,] Array = GetArray(rows, columns, 0, 10);
+PrintArray(Array);
+GetChengedArray(Array);
+Console.WriteLine();
+PrintArray(Array);
+
+//---------------------------------------------------------------------------
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] GetChengedArray(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+        int temp = array[0,j];
+        
+        
+        array[0, j] = array[array.GetLength(0) - 1, j];
+        array[array.GetLength(0) - 1, j] = temp;
+    }
+    return array;
+}
---
+++ CS_Seminar8/Task_57/Task_55.cs
@@ -0,0 +1,64 @@
+﻿// Составить частотный словарь элементов двумерного массива.
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных
+Console.Clear();
+int row = new Random().Next(1, 10);
+int column = new Random().Next(1, 10);
+// int row = 4;
+// int column = 4;
+int minRandomValue = new Random().Next(-9, 0);
+int maxRansomValue = new Random().Next(0, 10);
+int[,] array = GetArray(row, column, minRandomValue, maxRansomValue);
+
+PrintArray(array);
+
+Console.WriteLine();
+
+GetCheckArray(array);
+
+
+static int[,] GetArray(int row, int column, int minRandomValue, int maxRansomValue)
+{
+    int[,] randomArray = new int[row, column];
+
+    for (int rowIndex = 0; rowIndex < row; rowIndex++)
+    {
+        for (int columnIndex = 0; columnIndex < column; columnIndex++)
+        {
+            randomArray[rowIndex, columnIndex] = new Random().Next(minRandomValue, maxRansomValue);
+        }
+    }
+    return randomArray;
+}
+
+static void PrintArray(int[,] array)
+{
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+        for (int column = 0; column < array.GetLength(1); column++)
+        {
+            Console.Write($"{array[row, column]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+static void GetCheckArray(int[,] array)
+{
+    int count = 0;
+    int[] list = new int[array.GetLength(0) * array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            foreach (int el in array)
+            {
+                if (el == array[i, j])
+                {
+                    count ++;
+                }
+            }
+            Console.WriteLine($"{array[i, j]} встречается {count} раз");
+            count = 0;
+        }
+    }
+}
