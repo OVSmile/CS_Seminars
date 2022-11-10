2aae5e8eb754af167ae53d35ebfb40f4e082e377
Oleg Smile <valsov-9988@mail.ru>
2022-11-10 19:34:09 +0300

    Task_60

---
+++ CS_Seminar8/Task_60 Homework/Task_60 Homework.cs
@@ -0,0 +1,71 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, 
+// которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Console.Clear();
+
+Console.Write("Введите колличество строк массива: ");
+int row = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите колличество столбцов массива: ");
+int col = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите колличество строк в глубину массива: ");
+int dep = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine();
+
+
+if ((row * col * dep) > 100)
+    Console.WriteLine("Ошибка! Количество элементов в матрице не может превышать 100");
+else
+{
+    int[,,] array = CreateArray(row, col, dep, 1, 100);
+    PrintArray(array);
+}
+
+
+int[,,] CreateArray(int row, int col, int dep, int min, int max)
+{
+    int[,,] matrix = new int[row, col, dep];
+    Random rnd = new Random();
+    int t = 0;
+    //matrix[0,0,0] = t;
+    List<int> temp = new List<int>() { t };
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                while (true)
+                {
+                    t = rnd.Next(min, max + 1);
+                    if (temp.Contains(t))
+                        continue;
+                    else
+                    {
+                        temp.Add(t);
+                        matrix[i, j, k] = t;
+                        break;
+                    }
+                }
+
+            }
+        }
+    }
+    Console.WriteLine(String.Join(",", temp));
+    return matrix;
+}
+
+void PrintArray(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]}({i}, {j}, {k}) ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
---
+++ CS_Seminar8/Task_62 Homework/Task_62 Homework.cs
@@ -0,0 +1,44 @@
+﻿using System;
+ 
+ 
+public class Example
+{
+    public static void Main()
+    {
+        int N = 3, M = 3;
+        int[,] A = new int[N, M];
+ 
+ 
+        //Заносим значения в массив
+        
+        int row = 0, col = 0, dx = 1, dy = 0, dirChanges = 0, gran = M;
+        
+        for (int i = 0; i < A.Length; i++)
+        {
+            A[col, row] = i + 1;
+            if (--gran == 0)
+            {
+                gran = M * (dirChanges % 2) + N * ((dirChanges + 1) % 2) - (dirChanges / 2 - 1) - 2;
+                int temp = dx;
+                dx = -dy;
+                dy = temp;
+                dirChanges++;
+            }
+            col += dx;
+            row += dy;
+        }               
+ 
+        //Выводим на консоль
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < M; j++)
+            {
+                Console.Write(A[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+ 
+ 
+        Console.ReadKey();
+    }
+}
