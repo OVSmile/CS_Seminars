1e1d255c53fef332011ff21a6214752cc7a2c322
Oleg Smile <valsov-9988@mail.ru>
2022-11-11 00:40:05 +0300

    Task_62

--- CS_Seminar8/Task_62 Homework/Task_62 Homework.cs
+++ CS_Seminar8/Task_62 Homework/Task_62 Homework.cs
@@ -1,44 +1,76 @@
-﻿using System;
- 
- 
-public class Example
+﻿Console.Clear();
+
+Console.Write("Введите колличество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите колличество столбцов массива: ");
+int collumns = int.Parse(Console.ReadLine() ?? "");
+
+
+int[,] Array = new int[rows,collumns];
+
+int contourNumber = 1;
+int insideNamber = Array.GetLength(0) * 2 + Array.GetLength(1) * 2 - 3;
+
+
+void PrintArray(int[,] array)
 {
-    public static void Main()
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        int N = 3, M = 3;
-        int[,] A = new int[N, M];
- 
- 
-        //Заносим значения в массив
-        
-        int row = 0, col = 0, dx = 1, dy = 0, dirChanges = 0, gran = M;
-        
-        for (int i = 0; i < A.Length; i++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            A[col, row] = i + 1;
-            if (--gran == 0)
-            {
-                gran = M * (dirChanges % 2) + N * ((dirChanges + 1) % 2) - (dirChanges / 2 - 1) - 2;
-                int temp = dx;
-                dx = -dy;
-                dy = temp;
-                dirChanges++;
-            }
-            col += dx;
-            row += dy;
-        }               
- 
-        //Выводим на консоль
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < M; j++)
-            {
-                Console.Write(A[i, j] + " ");
-            }
-            Console.WriteLine();
+            Console.Write($"{array[i, j]} ");
         }
- 
- 
-        Console.ReadKey();
+        Console.WriteLine();
     }
 }
+
+void FillConturNumbers(int[,] array, int temp)
+{
+    for (int i = 0; i < 1; i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = temp;
+            temp++;
+        }
+    }
+    for (int j = array.GetLength(1) - 1; j < array.GetLength(1); j++)
+    {
+        for (int i = 1; i < array.GetLength(0); i++)
+        {
+            array[i, j] = temp;
+            temp++;
+        }
+    }
+    for (int i = array.GetLength(0) - 1; i < array.GetLength(0); i++)
+    {
+        for (int j = array.GetLength(1) - 2; j >= 0; j--)
+        {
+            array[i, j] = temp;
+            temp++;
+        }
+    }
+    for (int j = 0; j < 1; j++)
+    {
+        for (int i = array.GetLength(0) - 2; i > 0; i--)
+        {
+            array[i, j] = temp;
+            temp++;
+        }
+    }
+}
+void FillInsideNumbers(int row, int col, int temp)
+{
+    if (Array[row, col] == 0)
+    {
+        Array[row, col] = temp;
+        FillInsideNumbers(row, col + 1, temp + 1);
+        FillInsideNumbers(row + 1, col, temp + 1);
+        FillInsideNumbers(row, col - 1, temp + 1);
+        FillInsideNumbers(row - 1, col, temp + 1);
+    }
+}
+
+FillConturNumbers(Array, contourNumber);
+FillInsideNumbers(1, 1, insideNamber);
+PrintArray(Array);
